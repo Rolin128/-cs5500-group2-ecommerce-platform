97b55e2497145ea1b94290535e7c767593325a3a
anonymousmiaow <xushu2013@gmail.com>
2024-11-07 20:48:23 -0800

    Delete unuse part

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,23 +4,7 @@ from django.conf.urls.static import static  # Serve static and media files durin
 from django.contrib import admin  # Django admin site
 from django.urls import path, include  # Functions to define URL patterns
 
-# Define the URL patterns for the project
-urlpatterns = [
-    # Admin site URL
-    path('admin/', admin.site.urls),
 
-    # Store application URLs
-    path('', include('store.urls')),
-
-    # Cart application URLs
-    path('cart/', include('cart.urls')),
-
-    # Account application URLs
-    path('account/', include('account.urls')),
-
-    # Payment application URLs
-    path('payment/', include('payment.urls')),
-]
 
 # Serve media files during development
 if settings.DEBUG:
