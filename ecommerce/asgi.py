d873b31917db43b0f559bf3d9ab8a6ee244d6700
anonymousmiaow <xushu2013@gmail.com>
2024-11-07 20:52:55 -0800

    Config ASGI

--- ecommerce/asgi.py
+++ ecommerce/asgi.py
@@ -11,8 +11,4 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 
-# Set the default settings module for the 'ecommerce' Django project
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
-# Create the ASGI application callable for handling asynchronous requests
-application = get_asgi_application()
